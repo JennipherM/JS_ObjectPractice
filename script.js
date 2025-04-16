46e5156624ed09e1bed294d41438e07eac991331
Jennipher <jenniphermedina11@gmail.com>
2025-04-16 15:46:14 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//Object constructors  (function constructor)
+
+//object named Person -- first, last, age, eye are properties 
+
+function Person(first, last, age, eye) {
+    
+    //this references current object it is in
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+
+  //making Person objects
+  const myFather = new Person("John", "Doe", 50, "blue");
+  const myMother = new Person("Sally", "Rally", 48, "green");
+  const mySister = new Person("Anna", "Rally", 18, "green");
+  const mySelf = new Person("Johnny", "Rally", 22, "green");
+
+  document.getElementById("demo").innerHTML = myFather.firstName + " is " + myFather.age + " years old.";
+    //John is 50 years old.
